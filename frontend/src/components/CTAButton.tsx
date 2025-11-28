7560d4b3f48b3c3b91d0a660f4f6347762615e3e
otobongfp <otobongfp@gmail.com>
2025-11-29 00:47:34 +0100

    fixes

--- frontend/src/components/CTAButton.tsx
+++ frontend/src/components/CTAButton.tsx
@@ -1,22 +1,40 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { motion } from 'motion/react';
 
 interface CTAButtonProps extends React.ComponentProps<'button'> {}
 
-export const CTAButton = ({ className, children, ...props }: CTAButtonProps) => {
+export const CTAButton = ({ className, children }: CTAButtonProps) => {
   return (
-    <button
+    <motion.button
       className={cn(
-        'radial-gradient rounded-xs relative cursor-pointer px-8 py-4 transition-transform active:scale-95',
+        'radial-gradient rounded-xs relative cursor-pointer px-8 py-4',
         className,
       )}
-      {...props}
+      initial={{ '--x': '100%', scale: 1 }}
+      animate={{ '--x': '-100%' }}
+      whileTap={{ scale: 0.97 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: 'loop',
+        repeatDelay: 1,
+        type: 'spring',
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: 'spring',
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
     >
       <span className='linear-mask relative block h-full w-full font-normal tracking-wide text-white'>
         {children}
       </span>
       <span className='linear-overlay rounded-xs absolute inset-0 block p-[2px]' />
-    </button>
+    </motion.button>
   );
 };
